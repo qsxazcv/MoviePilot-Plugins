0a9a839b114ccb41ece55617fb05aee630f60d24
qsxazcv <qsxazcv@users.noreply.github.com>
2026-07-05 12:44:06 +0800

    chore(mediawarp): reset version baseline to 1.0.8

--- plugins.v2/mediawarp/__init__.py
+++ plugins.v2/mediawarp/__init__.py
@@ -32,7 +32,7 @@ class MediaWarp(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/refs/heads/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.0.10"
+    plugin_version = "1.0.8"
     # 插件作者
     plugin_author = "DDSRem"
     # 作者主页
